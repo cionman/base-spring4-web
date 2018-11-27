497667b00cad157065736bcf3eaeb958a2530596
cionman <topspin1278@naver.com>
2018-11-27 13:40:27 +0900

    인증시 BadCredentials 이벤트 발생시 info 로그에 request 주소입력

---
+++ src/main/java/com/base/common/component/auth/AuthenticationEventListeners.java
@@ -0,0 +1,32 @@
+package com.base.common.component.auth;
+
+import com.base.common.util.LogUtil;
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.EventListener;
+import org.springframework.security.authentication.event.AuthenticationFailureBadCredentialsEvent;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.text.MessageFormat;
+
+@Component
+public class AuthenticationEventListeners {
+    @Autowired
+    LogUtil logUtil;
+
+    @Autowired
+    private HttpServletRequest request;
+    /**
+     * BadCredential Exception 발생시 이벤트 발생
+     * @param event
+     */
+    @EventListener
+    public void handleBadCredentials(AuthenticationFailureBadCredentialsEvent event){
+        logUtil.info(MessageFormat.format("[로그인 실패][{0}]", request.getRemoteAddr()));
+    }
+}
