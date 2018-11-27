a737a38f3fa9c5574f12b2810611860ecb0bf160
cionman <topspin1278@naver.com>
2018-11-27 13:55:31 +0900

    인증시 성공 이벤트 메소드 작성

--- src/main/java/com/base/common/component/auth/AuthenticationEventListeners.java
+++ src/main/java/com/base/common/component/auth/AuthenticationEventListeners.java
@@ -5,6 +5,9 @@ import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
 import org.springframework.security.authentication.event.AuthenticationFailureBadCredentialsEvent;
+import org.springframework.security.authentication.event.AuthenticationFailureServiceExceptionEvent;
+import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
+import org.springframework.security.authentication.event.InteractiveAuthenticationSuccessEvent;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.context.request.RequestAttributes;
@@ -23,6 +26,16 @@ public class AuthenticationEventListeners {
     private HttpServletRequest request;
 
     /**
+     * Authentication 성공 시 이벤트 발생
+     * @param event
+     */
+    @EventListener
+    public void handleSuccessEvent(InteractiveAuthenticationSuccessEvent event){
+
+    }
+
+
+    /**
      * BadCredential Exception 발생시 이벤트 발생
      * @param event
      */
@@ -30,6 +43,5 @@ public class AuthenticationEventListeners {
     public void handleBadCredentials(AuthenticationFailureBadCredentialsEvent event){
         logUtil.info(MessageFormat.format("[로그인 실패][{0}][{1}]", request.getRemoteAddr(), event.getException().getMessage()));
     }
-
 
 }
